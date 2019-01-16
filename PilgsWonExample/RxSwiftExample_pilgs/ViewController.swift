099b1ee5808501108665ce6677a272337a3f3576
JHH <wjdgo813@naver.com>
2019-01-16 20:22:12 +0900

    pilgwon rx practice (1)

--- PilgsWonExample/RxSwiftExample_pilgs/ViewController.swift
+++ PilgsWonExample/RxSwiftExample_pilgs/ViewController.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-
+import RxCocoa
+import RxSwift
 /*
  예제
  https://pilgwon.github.io/blog/2017/09/26/RxSwift-By-Examples-1-The-Basics.html
@@ -20,10 +21,21 @@ class ViewController: UIViewController {
     private var shownCities = [String]()
     private let allCities = ["New York","London","Oslo","Warsaw","Berlin","Praga"]
     
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        
+        searchBar
+            .rx.text
+            .orEmpty //옵셔널이 아니도록
+            .debounce(0.5, scheduler: MainScheduler.instance) //0.5초 딜레이를 갖고
+            .distinctUntilChanged() //새로운 값이 이전과 같은지 비교
+            .filter{ !$0.isEmpty } //값이 비어 있을때는 빈 값을 요청하지 않기 위해
+            .subscribe({ [unowned self] query in 
+            print("\(query.element ?? "")")
+            self.shownCities = self.allCities.filter{ $0.hasPrefix(query.element ?? "")}
+            self.tableView.reloadData()
+        }).disposed(by: disposeBag)
     }
 }
 
